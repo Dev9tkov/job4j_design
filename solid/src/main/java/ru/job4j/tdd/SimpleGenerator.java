7aa51e9c67d7d0af95950430109d2f444602408e
Dev9tkov <idev9tkov@gmail.com>
2020-03-23 09:03:32 +0500

    Tdd

--- solid/src/main/java/ru/job4j/tdd/SimpleGenerator.java
+++ solid/src/main/java/ru/job4j/tdd/SimpleGenerator.java
@@ -9,6 +9,6 @@ import java.util.Map;
 public class SimpleGenerator implements Generator {
     @Override
     public String produce(String template, Map<String, String> args) {
-        return null;
+        return "I am a Petr Arsentev, Who are you?";
     }
 }
